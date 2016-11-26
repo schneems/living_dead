d396b38ad1c21862cd28b19a1d2b0b69cf6d1264
schneems <richard.schneeman@gmail.com>
2016-11-25 21:10:52 -0600

    Hello world

--- ext/living_dead/living_dead.c
+++ ext/living_dead/living_dead.c
@@ -6,8 +6,28 @@
 
 size_t rb_obj_memsize_of(VALUE obj); /* in gc.c */
 
+/*
+ *
+ *  call-seq:
+ *     LivingDead.trace(object)  -> nil
+ *
+ * Traces a specific object to see if it is retained or freed
+ *
+ */
+static VALUE
+living_dead_trace(VALUE self, VALUE obj)
+{
+    rb_p(obj);
+
+    return Qnil;
+}
+
+
 void
 Init_living_dead(void)
 {
+    VALUE mod = rb_const_get(rb_cObject, rb_intern("LivingDead"));
+
+    rb_define_module_function(mod, "trace", living_dead_trace, 1);
 
 }
