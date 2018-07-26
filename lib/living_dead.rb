d61fe4cbfab555f1ea71f88e3460e99294ebaf55
schneems <richard.schneeman+foo@gmail.com>
2018-07-26 10:09:25 -0500

    Attempt to be smarter about flushing
    Instead of picking a magic number and we can create an object in memory that is not retained, we then trace the object and while we detect that it is in memory we repeatedly flush the stack and create new objects. Once the object is out of memory then we can determine that it’s been removed by GC. 
    
    The theory here is that since the object we are tracing is more recent than the object that the user is tracing it will be more likely that GC will have cleared any unused objects before getting to the one we just created.
    
    It’s still not perfect, but hopefully it’s better than it was previously.

--- lib/living_dead.rb
+++ lib/living_dead.rb
@@ -18,32 +18,59 @@ module LivingDead
   end
 
   def self.traced_objects
-    gc_start
+    clear_garbage!
     tracing_hash.map do |_, trace|
       trace
     end
   end
 
-  private
-    def self.stack_flush(n = 100)
-       1.times { stack_flush(n-1) if n > 0 }
-    end
-
-    # https://github.com/schneems/heap_problem/pull/1
-    def self.gc_start
-      stack_flush
-      GC.start
-    end
-  public
-
   def self.freed_objects
-    traced_objects.select {|x| x.freed? }
+    traced_objects.select { |x| x.freed? }
   end
 
+private
+  # Here's the deal: Running GC.start does NOT guarantee
+  # that all "dead" objects will be cleared. Which is sad
+  # because this gem would have been really simple if that was
+  # the case.
+  #
+  # The way to work around that is by creating an object that is
+  # not retained and tracing it with ObjectTrace. We then
+  # must get Ruby to clear objects in the (object) heap and the stack
+  # to do that we iteratively create new objects and block objects
+  # until we can determine that the val that we made and traced is
+  # no longer retained by Ruby.
+  #
+  # When this happens we assume that any other dead objects that the
+  # user was watching are cleared as well.
+  #
+  # This is still not 100% guaranteed, but it's a good place to start.
+  #
+  # Some more context:
+  #   https://github.com/schneems/heap_problem/pull/1
+  def self.clear_garbage!
+    val       = Object.new
+    obj_trace = ObjectTrace.new(val)
+    val = nil
 
-
-
+    n = 100
+    while obj_trace.retained?
+      n += 1
+      block_stack_flush(n)
+      object_stack_flush(n)
     end
 
+    GC.start
+  end
+
+  def self.block_stack_flush(n)
+    1.times { block_stack_flush(n - 1) if n > 0 }
+  end
+
+  def self.object_stack_flush(n)
+    val = Object.new
+    val = nil
+    object_stack_flush(n - 1) if n > 0
+    nil
   end
 end
